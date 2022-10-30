bb99622067400781d1cfbb038bd28ccbca2872ed
IffatNur <iffatnoorshad00@gmail.com>
2022-10-30 10:00:22 +0600

    first commit

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA7mHDEtE0evSO04w1i542wS3j6ON7tpuo",
-  authDomain: "travello-client.firebaseapp.com",
-  projectId: "travello-client",
-  storageBucket: "travello-client.appspot.com",
-  messagingSenderId: "402722949776",
-  appId: "1:402722949776:web:86fd92fdfea0e5e80de615",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 // Initialize Firebase
